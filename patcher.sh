3ac84d99c5f3ee20d7e08c8703392dbd587ca76a
ApfelTV <32610623+ApfelTV@users.noreply.github.com>
2018-06-08 18:52:38 +0200

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 echo "Welcome to the Auto-DSiWare-Patcher! With this patcher you can patch a DSiWare .app and/or an NDS ROM (.nds)! Make sure that your NDS and/or app is in the same directory as this patcher.sh!"
 echo "You need mono, so make sure mono is installed!"
-echo "Smash sat Enter button to continue."
+echo "Press [ENTER] to continue."
 read
 clear
 echo "Loading..."
 sleep 2
 if [ ! -f ./WfcPatcher.exe ]; then
     clear
-    echo "FATAL ERROR! WfcPatcher.exe not found!"
+    echo "Error: WfcPatcher.exe not found!"
     echo "Exiting..."
     sleep 3
     clear
     exit 0
 fi
-echo "Searching for all dos .nds ROMS..."
+echo "Searching for .nds ROMS..."
 if [ ! -e *.nds ]
 then
     if [ ! -e *.app ]
 	then
 	    echo "No NDS Rom/ DSiWare app found!"
-		echo "Smash dat enter button to exit."
+		echo "Press [ENTER] to exit."
 		read
 		exit 0
     fi
@@ -48,7 +48,7 @@ then
 fi
 sleep 5
 clear
-echo "Patching complete, enjoy the interwebz on these old ass stale consoles, jk, I love the DS/DSi!"
-echo "Smash dat Enter buttom to GTFO!"
+echo "Patching complete! The patched binarys have a '(wiimmfi.de)' in their name, so if you want to use the patched ones, remove that '(wiimmfi.de)' and use them instead."
+echo "Press [ENTER] to exit!"
 read
 exit 0
