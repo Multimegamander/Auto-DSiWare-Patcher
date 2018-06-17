ce3c47620c68461cf3b3c55461bec5ef5349da6c
Multimegamander <gennt81@gmail.com>
2018-06-17 11:03:27 -0700

    Update and rename patcher.bat to Our_Lord_and_savior_Jesus_Christ.bat

---
+++ Our_Lord_and_savior_Jesus_Christ.bat
@@ -0,0 +1,63 @@
+@ECHO OFF
+TITLE SeNd Me HeNtAi
+COLOR 4F
+
+set /a exiting=10
+set /a repeat=1
+set /a errorrep=0
+set /a rep=0
+set /a cor=0
+set /a patchingnumber=1
+set /a patchingok=1
+set nonds=0
+cls
+color F0
+mode 150,55
+echo.
+echo.
+echo All my life I've been good
+echo Do what my mom and dad and God say I should
+echo Go to church and Bible School
+echo To live by God's rule
+echo So whatever people tell me
+echo That The Bible tells me
+echo I will do
+echo Walk the halls of high school with my purity ring
+echo Unlike those other girls, I've got my morals in check
+echo It was easy to do until I got a boyfriend
+echo And pardon my French, but he's cute as heck
+echo But I made a pact to keep my hymen intact
+echo And Jesus and I are tight
+echo Never learned about the birds and the bees
+echo I was taught to keep an aspirin in between my knees
+echo 'Cause The Bible says premarital sex is wrong
+echo But Jason says that guys can't wait that long
+echo I don't want to lose him to someone who'll do him
+echo I need to figure something out
+echo Well there's a loophole in The Scripture that works really well
+echo So I can get him off without going to hell
+echo It's my Hail Mary, full of grace
+echo In Jesus' name we go to fifth base!
+echo Oh, thank you for making me holy
+echo And thank you for giving me holes to choose from
+echo And since I'm not a godless whore
+echo He'll have to come in the back door
+echo Therefore
+echo Fuck me in the ass 'cause I love Jesus
+echo The good Lord would want it that way
+echo Gimme that sweet sensation of a throbbing rationalization
+echo It's just between you and me
+echo 'Cause everyone knows it's the sex that God can't see
+echo.
+echo.
+pause >NUL
+cls
+
+
+
+   )
+   echo  LOL!
+   echo Press [ENTER] to quit!
+   pause >NUL
+)
+goto exit_patcher
